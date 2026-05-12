ac5ce82c27932182a52b9062f67d8611229461e3
rakib3719 <bannah76769@gmail.com>
2026-05-12 17:49:29 +0600

    Feat: Navbar complate with responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
+import logo from "../../public/logo.svg";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -18,40 +20,17 @@ export default function Header() {
   ];
 
   return (
-    <header
-      className='w-full shadow-sm bg-white font-sans'
-      data-purpose='main-header'>
+    <header className='w-full  bg-white font-sans' data-purpose='main-header'>
       {/* Main Navigation Area */}
       <div
         className='flex flex-col md:flex-row w-full '
         data-purpose='logo-nav-container'>
         {/* Logo Section */}
         <div
-          className='bg-white flex items-center justify-between py-4 px-6 md:w-[250px] shrink-0'
+          className='bg-white flex items-center justify-between py-4 px-6 md:w-62.5 shrink-0'
           data-purpose='logo-section'>
           <Link href='/' className='flex flex-col items-center no-underline'>
-            <div className='flex font-bold text-2xl tracking-tight'>
-              <span className='text-secondary uppercase'>Errand</span>
-              <span className='text-primary uppercase relative ml-1'>
-                Hubb
-                <span className='absolute -left-[14px] top-[2px] opacity-20'>
-                  <svg
-                    fill='none'
-                    height='24'
-                    stroke='var(--color-primary)'
-                    strokeLinecap='round'
-                    strokeLinejoin='round'
-                    strokeWidth='2'
-                    viewBox='0 0 24 24'
-                    width='24'>
-                    <path d='M12 2v20M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6'></path>
-                  </svg>
-                </span>
-              </span>
-            </div>
-            <span className='text-[0.6rem] text-gray-500 italic mt-1 font-serif tracking-tighter'>
-              &quot;Getting your errands done ASAP!&quot;
-            </span>
+            <Image src={logo} alt='logo' />
           </Link>
 
           {/* Mobile Menu Button */}
@@ -95,7 +74,7 @@ export default function Header() {
             isMenuOpen ? "block" : "hidden"
           } md:flex bg-primary flex-1 items-center overflow-x-auto whitespace-nowrap transition-all duration-300 ease-in-out`}
           data-purpose='main-navigation'>
-          <ul className='flex flex-col md:flex-row w-full items-start md:items-center px-4 py-3 md:py-0 md:min-h-[60px]'>
+          <ul className='flex flex-col md:flex-row w-full items-start md:items-center px-4 py-3 md:py-0 md:min-h-15'>
             {navLinks.map((link, index) => (
               <li
                 key={link.name}
