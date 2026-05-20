8bfd99ffdca85700a9c3217f16052bfa8acab5a6
rakib3719 <bannah76769@gmail.com>
2026-05-20 16:54:26 +0600

    Feat: statshinfo complated

--- src/components/dashboard/client/MyPost/StatsInfo.tsx
+++ src/components/dashboard/client/MyPost/StatsInfo.tsx
@@ -12,8 +12,13 @@ export default function StatsInfo({
   totalPages,
 }: StatsInfoProps) {
   return (
-    <div className='flex justify-between items-center text-[10px] text-gray-400 uppercase tracking-widest font-bold'>
-      <div>{total} posts</div>
+    <div className='flex justify-between items-center text-[10px] text-gray-400 uppercase tracking-widest '>
+      <div className='flex items-center gap-1.5'>
+        <span className='inline-flex items-center justify-center bg-[#FDE2D1] text-[#EC6F27] w-8 h-8 rounded-full text-[10px] font-black'>
+          {total}
+        </span>
+        <span className=' font-normal text-[#6B6B6B]'>posts</span>
+      </div>
       <div>
         Page {currentPage} of {totalPages}
       </div>
